d8d98d91816717e06bf1cbb54e4edbd7e415f24e
Jeff Soares <jeffersonvsoares@hotmail.com>
2021-04-19 03:03:39 -0300

    Improved base layout

--- app/src/main/java/com/jx/calculator/MainActivity.kt
+++ app/src/main/java/com/jx/calculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jx.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import com.jx.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
